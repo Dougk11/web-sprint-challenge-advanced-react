d7b7b4f800f6cc3c8454eb714f7f595dca8dc68f
Douglas Kim <douglas.j.kim15@gmail.com>
2021-10-29 15:29:39 -0700

    finished mvp

--- interview_answers.md
+++ interview_answers.md
@@ -2,9 +2,10 @@
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. These will not be counted as a part of your sprint score but will be helpful for preparing you for your endorsement interview, and enhancing overall understanding.
 
 1. What are the main differences between a stateful and a functional component?
-
+    Stateful components can implement logic and state, and you can use React lifecycle methods inside class componenets. 
 2. When does a componentWillMount function be called? What about a componentWillUpdate?
-
+    componentWillMount is called right before rendering.  componentWillUpdate will run before re-rendering
 3. Define stateful logic.
-
+    Stateful logic is logic that is built and used within a component. Normally it deals with state inside the component
 4. What are the three step of creating a successful test? What is done in each phase?
+Arrange -> Act -> Assert. Arrange you are setting up the react component which you are testing, act is executing the tested behavior, and assert is extracting the response and seeing if it is what we expect it to be.
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,48 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+    render(<CheckoutForm />)
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", async () => {
+    render(<CheckoutForm />);
+    const firstNameField = screen.getByLabelText(/first name:/i);
+    userEvent.type(firstNameField, 'Doug');
+
+    const lastNameField = screen.getByLabelText(/last name:/i);
+    userEvent.type(lastNameField, 'Kim');
+
+    const addressField = screen.getByLabelText(/address:/i);
+    userEvent.type(addressField, '24 Hello Street');
+
+    const cityField = screen.getByLabelText(/city/i);
+    userEvent.type(cityField, 'Tinytown');
+
+    const stateField = screen.getByLabelText(/state:/i);
+    userEvent.type(stateField, 'Bigville');
+
+    const zipField = screen.getByLabelText(/zip:/i);
+    userEvent.type(zipField, '12345');
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const success = await screen.queryByTestID('successMessage')
+    expect(success).toBeInTheDocument();
+
+
+
+});
+
+firstName: "",
+  lastName: "",
+  address: "",
+  city: "",
+  state: "",
+  zip: "",
